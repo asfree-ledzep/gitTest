ad68d3335cf80dc1d53448067ffc316ec8c0dd7a
asfree-ledzep <asfree@naver.com>
2024-04-15 17:33:35 +0900

    5. aa

--- gitTest/src/main/java/TestServelt.java
+++ gitTest/src/main/java/TestServelt.java
@@ -26,8 +26,7 @@ public class TestServelt extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("first commit/bb");
-		
+		System.out.println("first commit/aa");		
 		
 	}
 
